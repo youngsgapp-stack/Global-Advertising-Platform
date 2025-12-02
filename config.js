8156cd8e512bd73fc9bfe049438bef102e01821c
youngsgapp-stack <youngs.gapp@gmail.com>
2025-12-02 15:45:11 +0900

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ export const CONFIG = {
     
     // Mapbox 설정
     MAPBOX: {
-        ACCESS_TOKEN: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+        ACCESS_TOKEN: 'pk.eyJ1IjoieW91bmc5MSIsImEiOiJjbWlvN2o1bjYwaGEzM2xvank4cjhrMGNzIn0.pk.eyJ1IjoieW91bmc5MSIsImEiOiJjbWlvN2o1bjYwaGEzM2xvank4cjhrMGNzIn0.dIefOUuYqwYtl8MwXbpJuw',
         STYLE: 'mapbox://styles/mapbox/dark-v11',
         DEFAULT_CENTER: [0, 20],
         DEFAULT_ZOOM: 2,
@@ -163,4 +163,5 @@ export const log = {
 };
 
 export default CONFIG;
+
 
